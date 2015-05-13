fb9aee046d4ae4e0862a85a3594801864aaee1a8
mfaizanattique <mfaizanattique@gmail.com>
2015-05-13 06:04:53 +0400

    initial draft
    added the initial draft and all the files

---
+++ js/app.js
@@ -0,0 +1,9 @@
+﻿(function () {
+    var app = angular.module('app', []);
+
+    app.controller('porfolio', function () {
+
+        this.persons = { name: 'Faizan', profession: 'Web developer' };
+
+    });
+})();
